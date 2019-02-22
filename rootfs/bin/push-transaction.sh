52635e8d2fe3ce4e88d102d82efdfdf5c56dcdda
analogic <info@analogic.cz>
2019-02-22 11:38:56 +0100

    Added lock to prevent message collision

--- rootfs/bin/push-transaction.sh
+++ rootfs/bin/push-transaction.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+lock="/var/run/bitcoind/events-in.lock"
+die() { echo "$*" 1>&2 ; exit 1; }
+
+(
+flock -x -w 3 200 || die "can't obtain lock $lock"
+
 /usr/bin/bitcoin-cli -conf=$1 gettransaction $2 | tr "\n" " " | timeout --signal=SIGTERM 1 sed "s/$/\n/"  > /var/run/bitcoind/events-in 2> /dev/null
+)200>$lock
+
 exit 0
