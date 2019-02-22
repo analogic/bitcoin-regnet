2fe04ce5009a0afcdcfdbe4e8f242c44bb264ad9
analogic <info@analogic.cz>
2019-02-22 13:57:00 +0100

    Update push-transaction.sh

--- rootfs/bin/push-transaction.sh
+++ rootfs/bin/push-transaction.sh
@@ -6,6 +6,7 @@ die() { echo "$*" 1>&2 ; exit 1; }
 (
 flock -x -w 3 200 || die "can't obtain lock $lock"
 
+echo "sending notice about tx $2"
 /usr/bin/bitcoin-cli -conf=$1 gettransaction $2 | tr "\n" " " | timeout --signal=SIGTERM 1 sed "s/$/\n/"  > /var/run/bitcoind/events-in 2> /dev/null
 )200>$lock
 
