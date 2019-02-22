c2791ad94a2204cc284d87062182664139732667
analogic <info@analogic.cz>
2019-02-22 13:56:36 +0100

    Update push-block.sh

--- rootfs/bin/push-block.sh
+++ rootfs/bin/push-block.sh
@@ -6,6 +6,7 @@ die() { echo "$*" 1>&2 ; exit 1; }
 (
 flock -x -w 3 200 || die "can't obtain lock $lock"
 
+echo "sending notice about block $1"
 timeout --signal=SIGTERM 1 echo $1 > $2 2> /dev/null
 )200>$lock
 
